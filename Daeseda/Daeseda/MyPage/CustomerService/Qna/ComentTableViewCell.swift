35859bb475305a988b16663bac0db16838e2b521
youngjoo <90439413+youngjoo00@users.noreply.github.com>
2023-10-23 18:15:11 +0900

    feat: 댓글 수정 구현

--- Daeseda/Daeseda/MyPage/CustomerService/Qna/ComentTableViewCell.swift
+++ Daeseda/Daeseda/MyPage/CustomerService/Qna/ComentTableViewCell.swift
@@ -7,9 +7,15 @@ class ComentTableViewCell: UITableViewCell {
     @IBOutlet weak var comentTextLabel: UILabel!
     @IBOutlet weak var comentTimeLabel: UILabel!
     @IBOutlet weak var comentDelBtn: UIButton!
+    @IBOutlet weak var comentEditBtn: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        comentDelBtn.layer.cornerRadius = 13
+        
         comentDelBtn.isHidden = true
+        comentEditBtn.isHidden = true
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
